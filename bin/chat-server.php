d573bfc365d4378ba959b8d4e8d6033bb11515c3
heyjo <heyjo@notvalid.com>
2019-09-15 14:49:46 -0300

    adding websocket to work on browser

--- bin/chat-server.php
+++ bin/chat-server.php
@@ -1,13 +1,18 @@
 <?php
 use Ratchet\Server\IoServer;
+use Ratchet\Http\HttpServer;
+use Ratchet\WebSocket\WsServer;
 use MyApp\Chat;
 
     require dirname(__DIR__) . '/vendor/autoload.php';
 
     $server = IoServer::factory(
-        new Chat(),
+        new HttpServer(
+            new WsServer(
+                new Chat()
+            )
+        ),
         8080
     );
 
     $server->run();
-    
